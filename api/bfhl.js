b893021b7021ab7033c3eb0aa9e9ac65e81fb7a8
unknown <chaturvedivimedha@gmail.com>
2025-08-29 11:37:02 +0530

    commit

---
+++ api/bfhl.js
@@ -0,0 +1,15 @@
+export default function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body.data || [];
+
+    res.status(200).json({
+      is_success: true,
+      user_id: "vimedha_chaturvedi_02102003",
+      email: "vimedha2@gmail.com",
+      roll_number: "22BCE7796",
+      received_data: data
+    });
+  } else {
+    res.status(200).send("Use POST method at /api/bfhl");
+  }
+}
